7b66c03f7749cf21534122615e20939e585d4b5d
Josh Rosen <joshr@z2live.com>
2012-01-18 06:15:45 -0800

    Initial Commit

---
+++ massexpand/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  massexpand
+//
+//  Created by Joshua Rosen on 1/18/12.
+//  Copyright (c) 2012 Z2Live. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
